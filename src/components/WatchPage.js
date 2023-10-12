f4906e73ce794a797872caf8a9f553b1d469e487
amarj132 <amarjadhav49@gmail.com>
2023-10-12 13:48:29 +0530

    autoplay added

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -22,7 +22,7 @@ const WatchPage = () => {
       <iframe
         width="1000"
         height="500"
-        src={"https://www.youtube.com/embed/" + searchParams.get("v") }
+        src={"https://www.youtube.com/embed/" + searchParams.get("v") + "?&autoplay=1&mute=0" }
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
